150def67219b5f5d6981936826f5f93f89831447
Dario Vladovic <d.vladimyr@gmail.com>
2019-05-28 06:48:17 +0200

    Implement `checkGithub` custom function :tada:
    - testing implementation using `gas-local`
    - syncing codebase using `@google/clasp`

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* global PropertiesService, UrlFetchApp, Utilities */
+
+var props = PropertiesService.getScriptProperties();
+
+function githubUrl() {
+  var args = [].slice.call(arguments, 0);
+  args.unshift('https://api.github.com');
+  args = args.map(function (arg) {
+    return arg.replace(/^\/+|\/+$/g, '');
+  });
+  return args.join('/');
+}
+
+function getUserRepos(username, githubToken) {
+  var authorization = Utilities.formatString('token %s', githubToken);
+  var headers = {
+    authorization: authorization,
+    accept: 'application/vnd.github.v3+json',
+    'user-agent': 'https://github.com/vladimyr/ee-summercamp-gapps-script'
+  };
+  var url = githubUrl('users', username, 'repos');
+  var resp = UrlFetchApp.fetch(url, { headers: headers });
+  return JSON.parse(resp.getContentText());
+}
+
+/**
+ * Check if user has target repo (e.g. 'ee-summercamp-2019')
+ * @param {string} username Github username
+ * @returns {boolean} boolean result
+ * @customfunction
+ */
+function checkGithub(username) {
+  if (username.map) {
+    return username.map(checkGithub);
+  }
+  var githubToken = props.getProperty('GITHUB_API_TOKEN');
+  var repoPattern = Utilities.formatString('^%s$', props.getProperty('GITHUB_REPO_PATTERN'));
+  var reRepoName = new RegExp(repoPattern, 'i');
+  var repos = getUserRepos(username, githubToken);
+  var match = repos.find(function (repo) {
+    return reRepoName.test(repo.name.trim());
+  });
+  return Boolean(match);
+}
---
+++ test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const gas = require('gas-local');
+const request = require('sync-request').default;
+const test = require('tape');
+
+const mocks = Object.assign({}, gas.globalMockDefault, {
+  PropertiesService: {
+    getScriptProperties: () => ({
+      getProperty: key => process.env[key]
+    })
+  },
+  UrlFetchApp: {
+    fetch(url, { method = 'get', headers }) {
+      const resp = request(method, url, { headers });
+      return {
+        getContentText: () => resp.body.toString()
+      };
+    }
+  }
+});
+
+const isRepo = repo => repo.hasOwnProperty('fork') &&
+  repo.hasOwnProperty('git_url') &&
+  repo.hasOwnProperty('ssh_url');
+
+test('load script', t => {
+  t.plan(1);
+  const script = gas.require('./src', mocks);
+  t.is(
+    typeof script.checkGithub, 'function',
+    'exports global `#checkGithub(username)` function'
+  );
+});
+
+test('create request url using `#githubUrl(...parts)`', t => {
+  const { githubUrl } = gas.require('./src', mocks);
+  t.plan(2);
+  t.is(
+    githubUrl('users', 'vladimyr'),
+    'https://api.github.com/users/vladimyr',
+    'creates correct api url'
+  );
+  t.is(
+    githubUrl('users/', '/vladimyr/'),
+    'https://api.github.com/users/vladimyr',
+    'correctly handles path separator/s'
+  );
+});
+
+test('fetch user repos', t => {
+  t.plan(2);
+  const { getUserRepos } = gas.require('./src', mocks);
+  const resp = getUserRepos('vladimyr', process.env.GITHUB_API_TOKEN);
+  t.assert(Array.isArray(resp), 'returns array');
+  const repo = resp[0];
+  t.assert(isRepo(repo), 'array contains user repos');
+});
+
+test('invoke `#checkGithub(username)`', t => {
+  t.plan(3);
+  const { checkGithub } = gas.require('./src', mocks);
+  let resp = checkGithub('vladimyr');
+  t.is(
+    typeof resp, 'boolean',
+    'returns `boolean`'
+  );
+  resp = checkGithub('santicevic');
+  t.true(resp, '@santicevic has target repo');
+  resp = checkGithub('ghost');
+  t.false(resp, '@ghost has NOT target repo');
+});
+
+test('invoke `#checkGithub([usernames])`', t => {
+  t.plan(3);
+  const { checkGithub } = gas.require('./src', mocks);
+  const resp = checkGithub(['santicevic', 'ghost']);
+  t.assert(Array.isArray(resp) && resp.length === 2, 'response is array');
+  t.true(resp[0], '@santicevic has target repo');
+  t.false(resp[1], '@ghost has NOT target repo');
+});
